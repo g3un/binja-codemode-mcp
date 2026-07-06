828ece42a330f447a42fe9ee5fdbad1f1558521f
Changeun Park <g3un@protonmail.ch>
2026-07-06 17:18:16 +0200

    chore(dev): standardize Nix development environment (#1)
    Reviewed-on: https://codeberg.org/g3un/binja-codemode-mcp/pulls/1

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "binja-codemode-mcp development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.python310
+              pkgs.uv
+            ];
+
+            shellHook = ''
+              export UV_PYTHON_DOWNLOADS=never
+            '';
+          };
+        });
+    };
+}
